bd57c88b58a19157fcef1ae198d9016ec7a235f1
Jannis R <mail@jannisr.de>
2020-09-23 13:13:09 +0200

    WIP: generate stable stop IDs

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 'use strict'
 
-// todo
+const {Client} = require('pg')
+const QueryStream = require('pg-query-stream')
+const stops = require('./lib/prepare-stable-ids/stops')
+
+;(async () => {
+	const db = new Client()
+	await db.connect()
+
+	const convert = async (query, onRow) => {
+		const stream = db.query(new QueryStream(query))
+		stream.on('data', onRow)
+		await new Promise((resolve, reject) => {
+			stream.once('end', resolve)
+			stream.once('error', reject)
+		})
+	}
+
+	console.error('stops')
+	await convert(stops.query, stops.onRow)
+	await db.end()
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
