eb026b8f9f1f809a6e2a6278cf26cf53b47ae8f5
Jannis R <mail@jannisr.de>
2020-09-27 19:53:03 +0200

    add arrivals_departures_with_stable_ids view

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const {readFileSync} = require('fs')
 const {Client} = require('pg')
 const QueryStream = require('pg-query-stream')
 const stops = require('./lib/prepare-stable-ids/stops')
 const routes = require('./lib/prepare-stable-ids/routes')
+
+const ARRS_DEPS_WITH_STABLE_IDS = readFileSync(require.resolve('./lib/arrivals_departures_with_stable_ids.sql'))
 
 ;(async () => {
 	const db = new Client()
@@ -26,6 +29,8 @@ const routes = require('./lib/prepare-stable-ids/routes')
 	await convert(stops)
 	console.error('routes')
 	await convert(routes)
+
+	process.stdout.write(ARRS_DEPS_WITH_STABLE_IDS)
 
 	await db.end()
 })()
