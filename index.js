5a35936f14bee943742f4dc2e054f44f40b3d896
Jannis R <mail@jannisr.de>
2020-10-24 16:14:08 +0200

    find & match movements, 2.1.0

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const {
 	matchDeparture,
 } = require('./lib/match-arrival-departure')
 const matchTrip = require('./lib/match-trip')
+const matchMovement = require('./lib/match-movement')
 
 module.exports = {
 	matchStop,
 	matchArrival,
 	matchDeparture,
 	matchTrip,
+	matchMovement,
 }
