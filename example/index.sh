ebb4c0dd116826a3daef828672030cd86fbc2fe0
Jannis R <mail@jannisr.de>
2020-11-29 23:52:39 +0100

    new examples 📝

---
+++ example/index.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+cd $(dirname $(realpath $0))
+set -x
+
+./match.js departure <dep-72-elbphilharmonie-2020-11-29.json
+./match.js departure <dep-rb81-wandsbek-2020-11-29.json
+./match.js departure <dep-s31-dammtor-2020-11-29.json
+./match.js departure <dep-u3-luebecker-str-2020-11-29.json
+./match.js trip <trip-re70-2020-11-20.json
+./match.js trip <trip-u1-2019-11-29.json
---
+++ example/match.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+'use strict'
+
+const {inspect} = require('util')
+const gtfsRtInfo = require('./gtfs-rt-info')
+const gtfsInfo = require('./gtfs-info')
+const createMatchStop = require('../lib/match-stop')
+const {
+	createMatchDeparture, createMatchArrival,
+} = require('../lib/match-arrival-departure')
+const createMatchTrip = require('../lib/match-trip')
+const createMatchMovement = require('../lib/match-movement')
+const MATCHED = require('../lib/matched')
+
+;(async () => {
+	const mode = process.argv[2]
+	let match
+	if (mode === 'stop') {
+		match = createMatchStop(gtfsRtInfo, gtfsInfo)
+	} else if (mode === 'departure') {
+		match = createMatchDeparture(gtfsRtInfo, gtfsInfo)
+	} else if (mode === 'arrival') {
+		match = createMatchArrival(gtfsRtInfo, gtfsInfo)
+	} else if (mode === 'trip') {
+		match = createMatchTrip(gtfsRtInfo, gtfsInfo)
+	} else if (mode === 'movement') {
+		match = createMatchMovement(gtfsRtInfo, gtfsInfo)
+	} else {
+		throw new Error('missing/invalid mode')
+	}
+
+	let unmatched = ''
+	for await (const chunk of process.stdin) unmatched += chunk
+	unmatched = JSON.parse(unmatched)
+
+	const matched = await match(unmatched)
+	console.log(inspect(matched, {depth: null, colors: true}))
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
