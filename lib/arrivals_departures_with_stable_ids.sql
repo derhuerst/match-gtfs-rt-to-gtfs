993061ef7aedf4c9fbd6d2775c25aecfb05f7268
Jannis R <mail@jannisr.de>
2021-03-11 13:53:28 +0100

    fix & optimize arrivals_departures_with_stable_ids 🐛
    - add station_stable_id
    - t_arrival/t_departure computation: follow GTFS Time semantics 🐛
    - respect station tz, stop tz & agency tz 🐛
    - trips.trip_id -> stop_times.trip_id to help PostgreSQL query planner

--- lib/arrivals_departures_with_stable_ids.sql
+++ lib/arrivals_departures_with_stable_ids.sql
@@ -4,22 +4,46 @@ SELECT
 	routes_stable.stable_id AS route_stable_id,
 	routes.route_short_name,
 	routes.route_type,
-	trips.trip_id,
+	stop_times.trip_id,
 	trips.trip_headsign,
 	trips.trip_short_name,
-	service_days.date,
+	"date",
 	stop_times.stop_sequence,
 	service_days.t_base,
-	service_days.t_base + stop_times.arrival_time AS t_arrival,
-	service_days.t_base + stop_times.departure_time AS t_departure,
+	(
+		make_timestamptz(
+			date_part('year', "date")::int,
+			date_part('month', "date")::int,
+			date_part('day', "date")::int,
+			12, 0, 0,
+			-- todo: insert fallback tz from JS
+			coalesce(station_timezone, stop_timezone, agency.agency_timezone, 'Europe/Berlin')
+		)
+		- interval '12 hours'
+		+ arrival_time
+	) t_arrival,
+	(
+		make_timestamptz(
+			date_part('year', "date")::int,
+			date_part('month', "date")::int,
+			date_part('day', "date")::int,
+			12, 0, 0,
+			-- todo: insert fallback tz from JS
+			coalesce(station_timezone, stop_timezone, agency.agency_timezone, 'Europe/Berlin')
+		)
+		- interval '12 hours'
+		+ departure_time
+	) t_departure,
 	stops.stop_id,
-	stops.stop_name,
 	stops.stable_id,
+	stops.stop_name,
 	stops.station_id,
+	stops.station_stable_id,
 	stops.station_name
 FROM stop_times
 LEFT JOIN stops_with_stations_and_stable_ids stops ON stop_times.stop_id = stops.stop_id
 LEFT JOIN trips ON stop_times.trip_id = trips.trip_id
 LEFT JOIN routes ON trips.route_id = routes.route_id
 LEFT JOIN routes_stable_ids routes_stable ON routes.route_id = routes_stable.route_id
+LEFT JOIN agency ON routes.agency_id = agency.agency_id
 LEFT JOIN service_days service_days ON trips.service_id = service_days.service_id;
