derhuerst/match-gtfs-rt-to-gtfs

commit b2fc9fcaeae61f323254fabc4bd807f57585e40d

tweak matching algorithm; 4.0.0

build-index.js
@@ -77,7 +77,8 @@ BEGIN;
 	await convert(routes)
 
 	process.stdout.write(`
-		CREATE INDEX ON trips (route_id, trip_id);
+CREATE INDEX ON trips (trip_id);
+CREATE INDEX ON trips (route_id);
 `)
 
 	process.stdout.write(ARRS_DEPS_WITH_STABLE_IDS)

lib/find-arrival-departure.js
@@ -30,10 +30,10 @@ const createFindArrDep = (gtfsRtInfo, gtfsInfo, type) => async (_) => {
 	debug('time frame', timeFrameStart, timeFrameEnd)
 
 	const dateStart = when
-	.minus({days: 3})
+	.minus({days: 1})
 	.toISODate()
 	const dateEnd = when
-	.plus({days: 3})
+	.plus({days: 1})
 	.toISODate()
 	debug('date range', dateStart, dateEnd)
 

lib/find-stop.js
@@ -30,6 +30,7 @@ const createFindStop = (gtfsRtInfo, gtfsInfo) => async ( _) => {
 			stop_stable_id = ANY(ARRAY[%L])
 			OR station_stable_id = ANY(ARRAY[%L])
 		)
+		LIMIT 1
 	`, ...[
 		stableIds,
 		[...stableIds, ...stationStableIds],

lib/prepare-stable-ids/routes.js
@@ -53,16 +53,16 @@ const createOnRoute = (gtfsRtInfo, gtfsInfo) => {
 
 const afterAll = `\
 \\.
+CREATE INDEX ON routes_stable_ids (route_id);
+CREATE INDEX ON routes_stable_ids (stable_id);
 
-CREATE MATERIALIZED VIEW routes_with_stable_ids AS
+CREATE VIEW routes_with_stable_ids AS
 SELECT
 	routes.*,
 	stable.stable_id
 FROM routes
 LEFT JOIN routes_stable_ids stable ON routes.route_id = stable.route_id
 ORDER BY route_id;
-
-CREATE INDEX ON routes_with_stable_ids (stable_ids);
 `
 
 module.exports = {

lib/prepare-stable-ids/stops.js
@@ -62,7 +62,7 @@ const afterAll = `\
 CREATE INDEX ON stops_stable_ids (stop_id);
 CREATE INDEX ON stops_stable_ids (stable_id);
 
-CREATE VIEW stops_with_stations_and_stable_ids AS
+CREATE MATERIALIZED VIEW stops_with_stations_and_stable_ids AS
 SELECT
 	stops.stop_id AS stop_id,
 	stop_stable.stable_id AS stop_stable_id,
@@ -96,6 +96,8 @@ LEFT JOIN stops_stable_ids station_stable ON stops.stop_id = station_stable.stop
 -- those with a parent_station first
 -- todo: make more explicit, use location_type?
 ORDER BY stops.parent_station;
+CREATE INDEX ON stops_with_stations_and_stable_ids (stop_stable_id);
+CREATE INDEX ON stops_with_stations_and_stable_ids (station_stable_id);
 `
 
 module.exports = {

commit 051359c2efe7c5f897a1e7a8c7ac3ddfe6778147

minor tweaks 📝; 6.4.0
The increased stable IDs indices' storage needs are caused by b2fc9fc.

build-index.js
@@ -16,7 +16,7 @@ if (argv.help || argv.h) {
 Usage:
     build-gtfs-match-index <path-to-gtfs-rt-info> <path-to-gtfs-info>
 Examples:
-    build-gtfs-match-index gtfs-rt.js gtfs.js
+    build-gtfs-match-index gtfs-rt.js gtfs.js | psql -b
 \n`)
 	process.exit(0)
 }

lib/db.js
@@ -10,5 +10,6 @@ const db = new Pool()
 // https://node-postgres.com/api/pool
 
 // todo: don't parse timestamptz into JS Date, keep ISO 8601 strings
+// https://github.com/brianc/node-pg-types
 
 module.exports = db